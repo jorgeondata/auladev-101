de48a884503d866979096640b03f21686fb64478
Jorge Lima <jorgevictorlima@gmail.com>
2025-03-14 20:42:53 +0000

    Adicionando página inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+    <h1>"Sangue, suor e lágrimas são necessários para qualquer plano de longo prazo funcione, desde que isso não entre em conflito com sua natureza."</h1>
+    <h2>Anônimo</h2>
+        </div>
+    )
+}
+
+export default Home;
