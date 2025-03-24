da11c4010c1d345c32ea863d277595d7df3e02d2
Jorge Lima <jorgevictorlima@gmail.com>
2025-03-24 21:18:29 +0000

    Backspace sem querer, no lugar errado

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
         <p>Suor, sangue e lágrimas são necessários para qualquer plano de longo prazo funcione,</p>
         <p>desde que isso não entre em conflito com sua natureza.</p>
     </h1>
-    <h2>Anônimo/h2>
+    <h2>Anônimo</h2>
 
         </div>
     )
