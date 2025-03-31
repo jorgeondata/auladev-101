24ce9fa5769292266244c4c8afaeaaa2b41b74d0
Jorge Lima <jorgevictorlima@gmail.com>
2025-03-31 23:00:32 +0000

    adiciona o prettier e o lintcheck-fix

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 function Home() {
-    return (
-        <div>
-    <h1>
-        <p>Suor, sangue e lágrimas são necessários para qualquer plano de longo prazo funcione,</p>
+  return (
+    <div>
+      <h1>
+        <p>
+          Suor, sangue e lágrimas são necessários para qualquer plano de longo
+          prazo funcione,
+        </p>
         <p>desde que isso não entre em conflito com sua natureza.</p>
-    </h1>
-    <h2>Anônimo</h2>
-
-        </div>
-    )
+      </h1>
+      <h2>Anônimo</h2>
+    </div>
+  );
 }
 
 export default Home;
